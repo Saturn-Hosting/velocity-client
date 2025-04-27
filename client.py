8de209b102646dcf499dd3f78ca43cc6f4034dc4
Pixako <fulgurgithub@gmail.com>
2025-04-28 05:12:27 +0900

    init

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+import json
+import select
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+with open('config.json') as f:
+    config = json.load(f)
+
+server.connect((config['host'], config['port']))
+
+while True:
+    socket_list = [sys.stdin, server]
+    read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
+
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            print(message)
+        else:
+            message = sys.stdin.readline()
+            server.send(message.encode())
+            sys.stdout.write("<You>")
+            sys.stdout.write(message)
+            sys.stdout.flush()
+server.close()
